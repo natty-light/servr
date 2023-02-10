f1ae952c5a1550273740eeceec343787bedc5c24
natty-light <75773163+natty-light@users.noreply.github.com>
2023-02-10 10:58:50 -0500

    Write Schools to filesystem - New func Utils.Write() - New type GenerateRequestBody - New dependency, google/uuid

--- controllers/generate.go
+++ controllers/generate.go
@@ -5,9 +5,21 @@ import (
 	"serveR/utils"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 )
 
+type GenerateRequestBody struct {
+	Schools []string `json:"schools" binding:"required"`
+}
+
 func GetGenerate(c *gin.Context) {
+
+	var request *GenerateRequestBody = &GenerateRequestBody{}
+	c.BindJSON(&request)
+
+	var id string = uuid.NewString()
+	utils.Write(request.Schools, id)
+
 	ch := make(chan error)
 
 	go func() {
--- utils/file.go
+++ utils/file.go
@@ -15,3 +15,22 @@ func Read() string {
 	}
 	return line
 }
+
+func Write(schools []string, id string) error {
+	var fileName string = id + ".csv"
+
+	f, err := os.Create(fileName)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	for _, school := range schools {
+		_, err := f.WriteString(school + ",\n")
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
