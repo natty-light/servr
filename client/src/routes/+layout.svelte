f7621ad7de52780f09008040cedcb0f39d0b5ecf
natty-light <75773163+natty-light@users.noreply.github.com>
2023-02-17 18:43:52 -0500

    Conditionally render login button - Dont render login button on login page

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -2,8 +2,10 @@
 	import { goto } from '$app/navigation';
   import { loggedIn } from '../stores';
   import "../app.css";
+	import { onMount } from 'svelte';
 
   let loginButtonText: string;
+  let showButton: boolean;
 
   const buttonClickHandler = (e: MouseEvent & { currentTarget: EventTarget & HTMLButtonElement;}) => {
     console.log($loggedIn)
@@ -14,17 +16,23 @@
       goto('/');
 	  } else {
       goto('/login');
+      showButton = false;
     }
   }
+  onMount(() => {
+    showButton = !(window.location.pathname === '/login');
+  })
 </script>
 
 
 <div id="header" class="flex flex-row pb-8 pt-2 mb-8 justify-end">
 	<nav>
-    <button class="border-2 mr-4 ml-4 pl-2 pr-2 rounded-sm"
-    on:click={(e) => buttonClickHandler(e)}>
-      {$loggedIn ? 'logout' : 'login'}
-    </button>
+    {#if showButton}
+      <button class="border-2 mr-4 ml-4 pl-2 pr-2 rounded-sm"
+      on:click={(e) => buttonClickHandler(e)}>
+        {$loggedIn ? 'logout' : 'login'}
+      </button>
+    {/if}
   </nav>
 </div>
 <slot>
